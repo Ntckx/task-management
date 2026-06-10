07f4656c02f0fd294cc75f780b4f0d32934b5a27
Ntckx <Nontakorn.chatk@gmail.com>
2026-06-10 23:31:30 +0700

    docs: add comments to clarify priority calculator mapping in PriorityStrategyRegistry

--- Strategy/PriorityStrategyRegistry.cs
+++ Strategy/PriorityStrategyRegistry.cs
@@ -5,6 +5,9 @@ namespace TaskManagement.Strategy;
 
 public class PriorityStrategyRegistry : IPriorityStrategyRegistry
 {
+    // * Return the given priority calculator for this task type
+    // * Used map to store Type as Key, and IPriority as value to provide the priority calculator of the type
+    // * Ex) BUG -> bugPriority
     private readonly Dictionary<TaskItemType, IPriority> _strategies;
     public PriorityStrategyRegistry(BugPriority bugPriority, FeaturePriority featurePriority, StoryPriority storyPriority)
     {
