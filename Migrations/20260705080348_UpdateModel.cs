473eb6193956a1b0553ecaaaed69ffad20f120a7
Ntckx <Nontakorn.chatk@gmail.com>
2026-07-05 15:09:57 +0700

    feat: [add] dockerize application with auto database migration

---
+++ Migrations/20260705080348_UpdateModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "StoryPoint",
+                table: "TaskItems",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "TaskItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoryPoint",
+                table: "TaskItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using TaskManagement.data;
 using TaskManagement.Exceptions;
 using TaskManagement.Factory;
+using TaskManagement.Observer;
+using TaskManagement.Repository;
+using TaskManagement.Services;
+using TaskManagement.State;
+using TaskManagement.Strategy;
 
 // Registers Service, Config, Logging, Controller, Database
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +17,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ITaskFactoryRegistry, TaskFactoryRegistry>();
+builder.Services.AddScoped<IPriorityStrategyRegistry, PriorityStrategyRegistry>();
+builder.Services.AddScoped<TaskStateRestorer>();
+builder.Services.AddScoped<List<ITaskObserver>>(provider => new List<ITaskObserver>
+{
+    new EmailObserver(),
+    new LogObserver()
+});
+builder.Services.AddScoped<BugPriority>();
+builder.Services.AddScoped<FeaturePriority>();
+builder.Services.AddScoped<StoryPriority>();
+
 builder.Services.AddScoped<BugFactory>();
 builder.Services.AddScoped<FeatureFactory>();
 builder.Services.AddScoped<StoryFactory>();
@@ -24,6 +43,11 @@ options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
 
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseExceptionHandler();
 app.UseStatusCodePages();
